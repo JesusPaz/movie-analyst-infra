b7b61c6ef6fcc688da9197b5c67ea625187df487
Jesus Paz <jeherpabo@gmail.com>
2020-04-03 17:13:00 -0500

    Creation of ansible files to deploy jenkins

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # movie-analyst-infra
+
+        terraform init
+        terraform plan -var-file="variables.tfvars" -auto-approve
+        terraform apply -var-file="variables.tfvars" -auto-approve
+        terraform destroy -var-file="variables.tfvars" -auto-approve
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "ami_id" {
+  type = "string"
+}
+
+variable "vpc_id" {
+  type = "string"
+}
+
+variable "region" {
+  type = "string"
+}
+
+variable "subnet_id" {
+  type = "string"
+}
+
+variable "instance_type" {
+  type = "string"
+}
+
+
+variable "bucket_name" {
+  type = "string"
+}
+
+variable "key" {
+  type = "string"
+}
+
+variable "instance_name" {
+  type = "string"
+}
+
+variable "user_name" {
+  type = "string"
+}
+
+variable "project_name" {
+  type = "string"
+}
+
+variable "jenkins_port" {
+  type = "string"
+}
